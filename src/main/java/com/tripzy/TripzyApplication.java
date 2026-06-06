684f52892870612c8540804b9e8dc001c74d5e15
Nayan kumar das <nayankumardas0604@gmail.com>
2026-06-06 20:36:02 +0530

    Add files via upload
    Initial Spring Boot setup

---
+++ src/main/java/com/tripzy/TripzyApplication.java
@@ -0,0 +1,13 @@
+package com.tripzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripzyApplication.class, args);
+	}
+
+}
